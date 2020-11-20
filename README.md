9708ccfe561961ad8e8b467d863deb23c1b7f6e9
Jose Maria <jmgcalabozo@gmail.com>
2020-11-20 19:50:38 +0100

    Minor updates

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # mypower
 Monitor house power consumption based on Mirubox
+
+Launch:
+docker stack deploy --compose-file docker-compose.yml mypower
+
+Stop:
+docker stack rm mypower
--- res/util/resetDB.sh
+++ res/util/resetDB.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker run -v ../../data_docker/data_docker/postgresql:/var/lib/postgresql/data -ti `docker images | grep postgres | cut -d ' ' -f1` /bin/su postgres /usr/lib/postgresql/11/bin/pg_resetwal -f /var/lib/postgresql/data
+docker run -v ~/coding/mypower/data_docker/postgresql:/var/lib/postgresql/data -ti `docker images | grep postgres | cut -d ' ' -f1` /bin/su postgres /usr/lib/postgresql/11/bin/pg_resetwal -f /var/lib/postgresql/data
 
 

b3c630475d42dca26c4146907b4abff93b8ccc3e
pi <pi@raspberry.com>
2023-12-28 16:01:48 +0100

    dumpdb

--- res/util/resetDB.sh
+++ res/util/resetDB.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker run -v ~/coding/mypower/data_docker/postgresql:/var/lib/postgresql/data -ti arm32v7/postgres:11.1 /bin/su postgres /usr/lib/postgresql/11/bin/pg_resetwal -f /var/lib/postgresql/data
+docker run -v ~/coding/mypower/data_docker/postgresql:/var/lib/postgresql/data -ti arm32v7/postgres:11.1 /bin/su -c "/usr/lib/postgresql/11/bin/pg_resetwal -f /var/lib/postgresql/data" postgres
 
 
